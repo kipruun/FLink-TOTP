432f0dcdc20e09e1ebd5c2e95ee6149a3e6b3c2e
VortexBinary-bis <t.grandclere@outlook.com>
2025-06-08 14:06:20 +0200

    Add commentaires

--- cli-flink.py
+++ cli-flink.py
@@ -4,49 +4,67 @@ import os
 import json
 
 
-def config_load():
-    path = os.path.dirname(os.path.abspath(__file__))
-    file_path = os.path.join(path, "config.json")
+def config_load(): # Load configuration
+    """Load configuration
+
+    Returns:
+        dict: The configuration
+    """
+    path = os.path.dirname(os.path.abspath(__file__)) # Get current file path
+    file_path = os.path.join(path, "config.json") # Add config file to path
+    
     if os.path.exists(file_path):
         with open(file_path, "r") as file:
             try:
-                json_file = json.loads(file.read())
+                json_file = json.loads(file.read()) # Load config as json
             except:
-                json_file = {}
+                json_file = {} # If json not corretly configured select empty dict
                 f = open(file_path, "w")
                 f.write("{}")
                 f.close()
-            if json_file.get("token"):
+                
+            if json_file.get("token"): # Load token
                 print("Token loadded:", json_file.get("token"))
             else:
                 print("Token not found.")
-            if json_file.get("timezone"):
+                
+            if json_file.get("timezone"): # Load timezone
                 print("Timezone loadded:", json_file.get("timezone")) 
             else:
                 print("Timezone not found")
+            
         return json_file
     return {}
 
 
-def config_save(timezone=False, secret=False):
+def config_save(timezone=False, secret=False): # Save a configuration
+    """Save config
+
+    Args:
+        timezone (str, optional): If filled it will save timezone. Defaults to False.
+        secret (ste, optional): If filled it will save token. Defaults to False.
+    """
     path = os.path.dirname(os.path.abspath(__file__))
     file_path = os.path.join(path, "config.json")
+    
     if os.path.exists(file_path):
         with open(file_path, "r") as file:
-            json_file = json.loads(file.read())
+            json_file = json.loads(file.read()) # load existing config
     else:
         json_file = {}
+        
     if secret:
-        json_file["token"] = secret
+        json_file["token"] = secret # Change / add token
+        
     if timezone:
-        json_file["timezone"] = timezone
+        json_file["timezone"] = timezone # Change / add timezone
     
     with open(file_path, "w") as f:
-        f.write(json.dumps(json_file))
+        f.write(json.dumps(json_file)) #save config
         
 
-init(autoreset=True)  
-os.system('cls' if os.name=='nt' else 'clear')
+init(autoreset=True) # Init colorama
+os.system('cls' if os.name=='nt' else 'clear') # Clear console
 
 header = f"""{Fore.CYAN}   ___ _ _       _      _____  ___  _____  ___ 
   / __\\ (_)_ __ | | __ /__   \\/___\\/__   \\/ _ \\
@@ -64,30 +82,34 @@ menu = f"""
 {Fore.CYAN}[2]{Fore.RESET} - Configuration
 {Fore.CYAN}[3]{Fore.RESET} - Exit
 """
+
 print(header, menu)
 
 
-config = config_load()
-if config == {} or config.get("token") == None:
+config = config_load() # load config and save it in a variable
+if config == {} or config.get("token") == None: # if token not found
     print(f"\n{Fore.RED}We couldn't find the secret in the config file.{Fore.RESET}")    
     secret = input("Enter the family link's code: ")
-    config_save(secret=secret)
+    config_save(secret=secret) # Save token given
 else:
-    secret = config.get("token")
+    secret = config.get("token") # Token found
     
 
 
 
-if config.get("timezone") != None:
+if config.get("timezone") != None: # if timezone found
     time_zone = config.get("timezone")
 else:
     time_zone = ""
-totp_element = TOTPElement(secret, timezone=time_zone)
+    
+totp_element = TOTPElement(secret, timezone=time_zone) # Init the Totp generator
 
 while True:
     choice = input(f"{Fore.CYAN}[?]> {Fore.RESET} ")
-    os.system('cls' if os.name=='nt' else 'clear')
-    print(header, menu)
+    
+    os.system('cls' if os.name=='nt' else 'clear') 
+    print(header, menu) # Update console
+    
     if choice == "0":
         print("Generating codes for all hours of the current day...")
         codes = totp_element.get_a_day_codes()
@@ -106,12 +128,15 @@ while True:
         print(f"{Fore.CYAN}[1]{Fore.RESET} - Change timezone")
         print(f"{Fore.CYAN}[2]{Fore.RESET} - Change token")
         conf_user_choice = input(f"{Fore.CYAN}[+]>{Fore.RESET}")
+        
         if conf_user_choice == "0":
             config = config_load()
+        
         elif conf_user_choice == "1":
             print("List of timezones: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List (tz identifiers)")
             timezone_user = input("Choose timezone: ")
             config_save(timezone=timezone_user)
+        
         elif conf_user_choice == "2":
             token_user = input("Enter your token: ")
             config_save(secret=token_user)
